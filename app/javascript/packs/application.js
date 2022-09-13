5355f2678a4e961f0de45bd52f71117284b8122a
Rodrigo - Xita rps <xitarps@gmail.com>
2022-09-13 00:49:51 -0300

    Add Bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap/dist/js/bootstrap.bundle"
+import "bootstrap/dist/css/bootstrap"
+import "./../src/application.css.scss"
 
 Rails.start()
 Turbolinks.start()
