8d27ecbea62ec35de1ac994de5dacc8a00db5455
Rodrigo - Xita rps <xitarps@gmail.com>
2022-09-13 00:40:27 -0300

    Add signed in user feature upload test

--- spec/features/user_upload_file_successfully_spec.rb
+++ spec/features/user_upload_file_successfully_spec.rb
@@ -4,12 +4,15 @@ require 'rails_helper'
 
 RSpec.describe 'User' do
   describe 'upload file' do
+    let(:user){ User.create(email: 'tester@tester.com', password: 123456) }
     let(:tab_test_file){ file_fixture("example_input.tab")}
     context 'when correct file' do
       it 'success' do
         result = TabUploadService.call(tab_test_file.read )
         upload_value = result[:uploads_values][:file_gross_income].to_s.gsub('.',',')
         all_uploads_value = result[:uploads_values][:total_gross_income].to_s.gsub('.',',')
+
+        login_as user, scope: :user
 
         visit root_path
         click_on :Envio
@@ -30,6 +33,8 @@ RSpec.describe 'User' do
         upload_value = result[:uploads_values][:file_gross_income]
         all_uploads_value = result[:uploads_values][:total_gross_income]
 
+        login_as user, scope: :user
+
         visit root_path
         click_on :Envio
 
@@ -39,6 +44,19 @@ RSpec.describe 'User' do
         expect(page).not_to have_content(all_uploads_value)
         expect(page).not_to have_content('Total do upload')
         expect(page).to have_content('Error on file upload')
+      end
+    end
+
+    context 'when not signed in' do
+      it 'failure' do
+        result = TabUploadService.call(tab_test_file.read )
+        upload_value = result[:uploads_values][:file_gross_income]
+        all_uploads_value = result[:uploads_values][:total_gross_income]
+
+        visit upload_path
+        
+        expect(page).not_to have_content(:Salvar)
+        expect(page).to have_content('efetue login ou registre-se')
       end
     end
   end
